aca1b834c54d5c7186a5bcc3c3777ea0b093e20e
skander Ben achour <benachour.skander1@gmail.com>
2025-05-06 19:55:46 +0100

    added search engine for pics to give a pic to each city searched and made the page better

--- weatherapp/views/home_view.py
+++ weatherapp/views/home_view.py
@@ -1,30 +1,58 @@
-
 import os
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.contrib import messages
 import requests
 import datetime
 
-OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
-
 
 def home_view_render(request):
+    
+    OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY', '')
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', '')
+    SEARCH_ENGINE_ID = os.getenv('SEARCH_ENGINE_ID', '')
+   
     if 'city' in request.POST:
-        city= request.POST['city']
-    else :
-        city ="sousse"
-        
+         city = request.POST['city']
+    else:
+         city = 'sousse'     
     
-    print(OPENWEATHER_API_KEY)
-    url= f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}'
-    PARAMS= {'units':'metric'}
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}'
+    PARAMS = {'units':'metric'}
+     
+    query = city + " 1920x1080"
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+
+    data = requests.get(city_url).json()
+    count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
+    
+
+    try:
+          
+          data = requests.get(url,params=PARAMS).json()
+          description = data['weather'][0]['description']
+          icon = data['weather'][0]['icon']
+          temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'home.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
+    
+    except KeyError:
+          exception_occurred = True
+          messages.error(request,'Entered data is not available to API')   
+          # city = 'indore'
+          # data = requests.get(url,params=PARAMS).json()
+          
+          # description = data['weather'][0]['description']
+          # icon = data['weather'][0]['icon']
+          # temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'home.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'indore' , 'exception_occurred':exception_occurred } )
+               
     
     
-    data= requests.get(url, PARAMS).json()
-    
-    
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temperature = data['main']['temp']
-    print(temperature)
-    date = datetime.date.today()
-    return render(request, 'home.html', {'description': description, 'icon': icon, 'temperature': temperature, 'day': date})
